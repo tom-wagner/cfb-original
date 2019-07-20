b7a77d8e087a0a82b1a40c03017f81485b8a3db1
Tom Wagner <tom.wagner@xaxis.com>
2019-07-20 14:55:40 -0400

    Initial reconfiguration

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <header>
+        <p>Hello world</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
