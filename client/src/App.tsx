4499ad7413f2716a7c447630108bcc7a20595036
Tom Wagner <tom.wagner@xaxis.com>
2019-07-20 15:35:36 -0400

    Configure basic flask endpoint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+const axios = require('axios');
 
 const App: React.FC = () => {
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:5000/")
+      .then((data: string) => {
+        console.log('is flask working??', data)
+      })
+  }, [])
+
   return (
     <div>
       <header>
