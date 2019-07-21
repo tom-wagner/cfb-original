5a629e017b08b45c9b2b4e27b3d8eac6e3816de0
Tom Wagner <tom.wagner@xaxis.com>
2019-07-21 15:23:53 -0400

    Add teams and schedule routes that call CollegeFootballData api

---
+++ server/external_apis/cf_data.py
@@ -0,0 +1,13 @@
+import requests
+
+BASE_CFD_API = "https://api.collegefootballdata.com"
+
+
+class CFData:
+    def __init__(self):
+        self.base_path = BASE_CFD_API
+
+    def get(self, route: str, **params):
+        resp = requests.get(f'{self.base_path}/{route}', params=params)
+        return resp
+
